8ab2afc129a598f59b5843cbcf7b04ea9d7513b4
olatocode <awosolat@gmail.com>
2025-07-24 09:53:38 +0100

    fix: Fix conflicting registration error

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,13 +8,31 @@ import { prisma } from '../config/db';
 export const registerUser = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
   try {
+    // Check for existing username or email
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ username }, { email }],
+      },
+    });
+    if (existingUser) {
+      let conflictField =
+        existingUser.username === username ? 'username' : 'email';
+      return res
+        .status(409)
+        .json({ message: `A user with this ${conflictField} already exists.` });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
       data: { username, email, password: hashedPassword },
     });
     res
       .status(201)
-      .json({ message: 'Registration successfuly', data: user.id, username: user.username , email: user.email });
+      .json({
+        message: 'Registration successfuly',
+        data: user.id,
+        username: user.username,
+        email: user.email,
+      });
   } catch (err) {
     res.status(500).json({ message: 'Error creating user', error: err });
   }
@@ -34,7 +52,7 @@ export const loginUser = async (req: Request, res: Response) => {
         expiresIn: '1d',
       }
     );
-      res.status(200).json({ message: "Login successful" ,token });
+    res.status(200).json({ message: 'Login successful', token });
   } catch (err) {
     res.status(500).json({ message: 'Login error', error: err });
   }
